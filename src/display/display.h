40129937c1a64a721b897f65b412a5aa927967b0
Jking04 <jamestrking04@gmail.com>
2026-08-30 21:00:22 +0200

    network reading, and some more refactors

--- src/display/display.h
+++ src/display/display.h
@@ -5,6 +5,7 @@
 #include "cpu.h"
 #include "mem.h"
 #include "temp.h"
+#include "network.h"
 #include "bar.h"
 
-void tuiDisplay(CpuData&,MemData&,TempData&);
+void tuiDisplay(CpuData&,MemData&,TempData&,NetData&,double);
